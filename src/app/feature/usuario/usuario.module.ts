1c6a0bbbc4e1a045731dbd35c81b879c1d48887c
Ceiba-NelsonGarcia <nelson.garcia@ceiba.com.co>
2022-04-21 16:12:42 -0500

    1 Cargue inicial del proyecto base con el componente usuario en feature

---
+++ src/app/feature/usuario/usuario.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListarUsuariosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsuarioModule { }
