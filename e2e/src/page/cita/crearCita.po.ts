c3af02caf21eeec40393a9a2045b694aa8789384
Ceiba-NelsonGarcia <nelson.garcia@ceiba.com.co>
2022-05-05 18:30:47 -0500

    16 Actualización de los Id del formulario crear-cita.html

--- e2e/src/page/cita/crearCita.po.ts
+++ e2e/src/page/cita/crearCita.po.ts
@@ -2,11 +2,11 @@ import {by, element} from 'protractor';
 
 export class CrearCitaPage {
 
-  private inputIdUsuario = element(by.id('idUsuario'));
-  private inputIdDoctor = element(by.id('idDoctor'));
-  private inputFechaCita = element(by.id('fechaCita'));
-  private inputHoraInicial = element(by.id('horaInicial'));
-  private inputHoraFinal = element(by.id('horaFinal'));
+  private inputIdUsuario = element(by.id('id_Usuario'));
+  private inputIdDoctor = element(by.id('id_Doctor'));
+  private inputFechaCita = element(by.id('fecha_Cita'));
+  private inputHoraInicial = element(by.id('hora_Inicial'));
+  private inputHoraFinal = element(by.id('hora_Final'));
 
   private linkAgregar = element(by.id('agregar'));
   private linkAtras = element((by.id('atras')));
